b1e357fc7abc8dd00f6b901d5a7d31fe4be8e635
Adriano Todaro <a.todaro.lcg@gmail.com>
2020-01-13 21:36:26 +0100

    fix question serializer behaviour

--- apps/games/serializers.py
+++ apps/games/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
 from apps.games.models import Question
 from apps.songs.serializers import SongSerializer
+from apps.songs.models import Song
 
 
 class QuestionSerializer(serializers.ModelSerializer):
-    song = SongSerializer()
+    song = SongSerializer(read_only=True)
+    song_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = Question
-        fields = ['song', 'suggestion', 'answer']
+        fields = ['song', 'suggestion', 'answer', 'song_id']
+
+    def create(self, validated_data):
+        song_id = validated_data.pop('song_id')
+        song = Song.objects.get(pk=song_id)
+        question = Question.objects.create(song=song, **validated_data)
+        return question
+
